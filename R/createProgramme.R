75b71bce2a8a6ce51000c036a76dd81348494b2a
stevenjwest <stevenjonwest@gmail.com>
2019-11-07 08:30:52 +0000

    Remove project creation
    Removed the creation of a Project from this function, and also added appropriate log comments for the creation of directories and files.

--- R/createProgramme.R
+++ R/createProgramme.R
@@ -7,8 +7,7 @@
 #'
 #' @export
 createProgramme <- function(fileSystemPath, programmeName, programmeTitle,
-                             programmePrefix, projectName, projectTitle,
-                             programmeIndex=0) {
+                             programmePrefix, programmeIndex=0) {
 
 
   # Check fileSystemPath is at the root of an ORGANISATION:
@@ -20,35 +19,57 @@ createProgramme <- function(fileSystemPath, programmeName, programmeTitle,
   while(  !( file.exists(confPath) && file.exists(tempPath) )  ) {
     fileSystemPath <- dirname(fileSystemPath)
     if(nchar(fileSystemPath) <=1) {
-      stop( paste("Could not identify ORGANISATION in fileSystemPath: ",fileSystemPath, sep="") )
+      stop( cat("Could not identify ORGANISATION in fileSystemPath: ",fileSystemPath, "\n") )
     }
     confPath = paste(fileSystemPath, .Platform$file.sep, ".config" , sep="")
-    tempPath = paste(fileSystemPath, .Platform$file.sep, "templates" , sep="")
+    tempPath = paste(fileSystemPath, .Platform$file.sep, "templates", sep="")
   }
 
   # now fileSystemPath should contain the path to the ORG ROOT DIR
 
+
   # read all DIRs in fileSystemPath:
 
-  if(programmeIndex < 1 ) { # if programmeIndex is below 1 (default is 0), then try to identify what programmeIndex should be by looking at DIR numbers:
+  if(programmeIndex < 1 ) { # if programmeIndex is below 1 (default is 0),
+                            # then try to identify what programmeIndex should be by looking at DIR numbers:
 
     directories <- dir(fileSystemPath, recursive = FALSE, full.names = FALSE, pattern="*[0-9]{1-6}[-]{1}")
+
     programmeIndexes <- sapply( directories, function(x) substr(x, 0, gregexpr("-", x)[[1]][1]-1) )
+
     programmeIndex <- sort( as.numeric(programmeIndexes) )[length(programmeIndexes)]
+
     programmeIndex <- programmeIndex+1
+
+    if(length(programmeIndex) == 0 ) {
+
+      programmeIndex <- 1 #this ensures if there are NO directories that match the glob above, that the index is set to 1!
+
+    }
 
     # if programmeIndex is only one digit, append "0" to front:
     if(programmeIndex < 10 ) {
+
       programmeIndex <- paste("0", programmeIndex, sep="")
+
     } else {
+
       programmeIndex <- paste("", programmeIndex, sep="")
+
     }
+
   } else { # else, if programmeIndex was set to be above 0, then use this number!
+
     if(programmeIndex < 10 ) {
+
       programmeIndex <- paste("0", programmeIndex, sep="")
+
     } else {
+
       programmeIndex <- paste("", programmeIndex, sep="")
+
     }
+
   }
 
 
@@ -56,23 +77,46 @@ createProgramme <- function(fileSystemPath, programmeName, programmeTitle,
 
   # create Dir:
   progPath = paste(fileSystemPath, .Platform$file.sep, programmeIndex, "-", programmeName, sep="")
-  dir.create(progPath)
+  done <- dir.create(progPath)
+
+  if(!done) {
+    stop( cat("Programme directory could not be created: ", progPath, "\n") )
+  }
+
+  cat( "Made Programme dir: ",progPath, "\n" )
+
 
   # create PROJECTS dir:
   projsPath = paste(progPath, .Platform$file.sep, "PROJECTS", sep="")
-  dir.create(projsPath)
+  done <- dir.create(projsPath)
+
+  if(!done) {
+    stop( cat("PROJECTS directory could not be created: ", projsPath, "\n") )
+  }
+
+  cat( "  Made PROJECTS dir: ",projsPath, "\n" )
+
 
   # create SOP dir:
   sopPath = paste(progPath, .Platform$file.sep, "SOP", sep="")
   dir.create(sopPath)
+
+  if(!done) {
+    stop( cat("SOP directory could not be created: ", sopPath, "\n") )
+  }
+
+  cat( "  Made SOP dir: ",sopPath, "\n" )
+
 
   # create Rmd file:
   progFile = paste(progPath, .Platform$file.sep, "index.Rmd", sep="")
   done <- file.create(progFile)
 
   if(!done) {
-    stop( paste("Programme file could not be created: ", progFile, sep="") )
+    stop( cat("Programme index.md file could not be created: ", progFile, "\n") )
   }
+
+  cat( "  Made Programme index.md file: ",progFile, "\n" )
 
 
   # Create a config file for this Programme:
@@ -80,33 +124,6 @@ createProgramme <- function(fileSystemPath, programmeName, programmeTitle,
   names(programme) <- c("programmeName", "programmePrefix")
   yaml::write_yaml( yaml::as.yaml(programme), paste(confPath, .Platform$file.sep, programmeName, ".yaml", sep="") )
 
-
-  ### CREATING A PROJECT: ###
-
-  # create Dir:
-  projPath = paste(projsPath, .Platform$file.sep, programmePrefix, "01", sep="")
-  dir.create(projPath)
-
-  # create Rmd file:
-  projFile = paste(progPath, .Platform$file.sep, "PROJECTS", .Platform$file.sep, programmePrefix, "01~_", projectName, ".Rmd", sep="")
-  done <- file.create(projFile)
-
-  if(!done) {
-    stop( paste("Project file could not be created: ", projFile, sep="") )
-  }
-
-  # read project doc template:
-  templateFileConn <- file( paste( tempPath, .Platform$file.sep, "Project-Doc-Template.Rmd", sep="") )
-  templateContents <- readLines( templateFileConn )
-  close(templateFileConn)
-
-  # modify templateContents to include PREFIX and projectTitle
-  templateContents <- gsub("{{PREFIX}}", paste(programmePrefix, "01", sep=""), templateContents, fixed=TRUE)
-  templateContents <- gsub("{{TITLE}}", projectTitle, templateContents, fixed=TRUE)
-
-  # write to projFile
-  fileConn <- file(projFile)
-  writeLines(templateContents, fileConn)
-  close(fileConn)
+  cat( "  made Programme index.md file: ",progFile, "\n" )
 
 }
