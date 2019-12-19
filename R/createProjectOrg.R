98c384770a1f48f069035063934095990a38a2b4
stevenjwest <stevenjonwest@gmail.com>
2019-12-19 21:31:11 +0000

    Add site, config, template dirs
    Added site, config, template dirs when creating a new Organisation.  Have removed support for HUGO for now, and plan to just use R Markdown for compiling RMD files to html.

--- R/createProjectOrg.R
+++ R/createProjectOrg.R
@@ -8,8 +8,7 @@
 #' as well as copy several default templates for Project Docs and Notes.
 #'
 #' @export
-createProjectOrg <- function(fileSystemPath=getwd(), orgName = "00_ORG",
-                             orgTitle = "ORGANISATION", siteName = "00_SITE") {
+createProjectOrg <- function(fileSystemPath=getwd(), orgName = "00_ORG", orgTitle = "ORGANISATION") {
 
 
   ### CREATING THE ORGANISATION: ###
@@ -25,6 +24,42 @@ createProjectOrg <- function(fileSystemPath=getwd(), orgName = "00_ORG",
   cat( "Made ORG dir: ",orgPath, "\n" )
 
 
+
+  ### CREATE THE SITE DIRECTORY ###
+
+  # This will contain the compiled HTML - which will be made using the R Markdown in the first instance
+  # In future, want to move to using HUGO and Blogdown potentially?
+
+  # Do NOT make this a Hidden directory - as the user will want to access it!
+
+  # site dir:
+  sitePath = paste(orgPath, .Platform$file.sep, "site" , sep="")
+  done <- dir.create( sitePath )
+
+  if(!done) {
+    stop( cat("Site directory could not be created: ", sitePath, "\n") )
+  }
+
+  cat( "Made site dir: ", sitePath, "\n" )
+
+
+  ### CREATE CONFIG DIRECTORY ###
+
+  # Contain configuration information, mainly for each Programme information on incomplete notes for To Do lists
+
+  # DO NOT make this a Hidden Driectory - as User may want to look in here?
+
+  # config dir:
+  confPath = paste(orgPath, .Platform$file.sep, "config" , sep="")
+  done <- dir.create( confPath )
+
+  if(!done) {
+    stop( cat("Config directory could not be created: ", confPath, "\n") )
+  }
+
+  cat( "Made config dir: ", confPath, "\n" )
+
+
   # templates Dir:
   tempPath = paste(orgPath, .Platform$file.sep, "templates" , sep="")
   done <- dir.create( tempPath )
@@ -35,24 +70,12 @@ createProjectOrg <- function(fileSystemPath=getwd(), orgName = "00_ORG",
 
   cat( "Made templates dir: ",tempPath, "\n" )
 
-
-  # hidden .config dir:
-  confPath = paste(orgPath, .Platform$file.sep, ".config" , sep="")
-  done <- dir.create( confPath )
-
-  if(!done) {
-    stop( cat("Config directory could not be created: ", confPath, "\n") )
-  }
-
-  cat( "Made config dir: ", confPath, "\n" )
-
-
   # copy template files:
     # need to copy from the PACKAGE!
-  templateDir <- paste( find.package("projectmanagr"), .Platform$file.sep, "templates", .Platform$file.sep, "Project-Doc-Template.Rmd", sep="")
+  templateDir <- paste( find.package("projectmanagr"), .Platform$file.sep, "templates", .Platform$file.sep, sep="")
   templateRmds <- list.files(templateDir)
   for(f in templateRmds) {
-    done <- file.copy(paste(templateDir, f, sep=""), tempPath)
+    done <- file.copy( paste(templateDir, f, sep=""), tempPath)
     if(!done) {
       stop( cat("  Copied Template: ", f, "\n") )
     }
@@ -69,20 +92,5 @@ createProjectOrg <- function(fileSystemPath=getwd(), orgName = "00_ORG",
   }
 
   cat( "Made config dir: ",confPath, "\n" )
-
-
-
-  ### CREATING THE HUGO SITE: ###
-
-  # create Dir:
-  sitePath = paste(fileSystemPath, .Platform$file.sep, siteName , sep="")
-  dir.create( sitePath )
-
-  if(!done) {
-    stop( cat("Site directory could not be created: ", sitePath, "\n") )
-  }
-
-  cat( "Made SITE dir: ",sitePath )
-
 
 }
