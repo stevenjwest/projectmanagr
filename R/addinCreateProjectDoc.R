9b262dc18994deb98bf7a56732f0ffb9dbe9ad8a
stevenjwest <stevenjonwest@gmail.com>
2020-01-06 12:23:50 +0000

    Add addinCreateProjectDoc function
    This provides a shiny interface to adding a new Project Doc via RStudio Addins.  The addin checks the inputs are valid before running createProjectDoc().

---
+++ R/addinCreateProjectDoc.R
@@ -0,0 +1,181 @@
+#' Create a New Project Document
+#'
+#' Generates a Shiny Gadget for creating a new Project Doc inside
+#' a Programme.  User selects a destination in
+#' the file system (MUST be a Programme Dir), Project name, and
+#' Project title (for the html page).
+#'
+#' Stipulates any errors in the input, and can only be completed
+#' when these errors have been resolved.
+#'
+#'
+#' @export
+addinCreateProjectDoc <- function() {
+
+  ui <- miniPage(
+
+    gadgetTitleBar("Create New Project Document"),
+
+    miniContentPanel(
+
+      fillCol(
+
+        fillRow( h5("Create a new Project Document inside a Programme.") ),
+
+        fillRow(  textInput("projectName", "Project Name:", value = "PROJECT", width="100%")  ),
+
+        fillRow(  span( textOutput("warningName"), style="color:red")  ),
+
+        fillRow(  textInput("projectTitle", "Project Title:", value = "01 PROGRAMME", width="100%")  ),
+
+        fillRow( flex = c(7, 1),  verbatimTextOutput("dir", placeholder = TRUE), shinyDirButton("dir", "Select Directory", "Programme Parent Directory")  ),
+
+        fillRow(   span( textOutput("warningDirectory"), style="color:red")  ),
+
+        fillRow(   textOutput("projectPathOutput")  )
+
+      )
+    )
+  )
+
+  server <- function(input, output, session) {
+
+    # update programmeTitle when programmeName is changed:
+    observe({
+
+      updateTextInput(session, "projectTitle", value = gsub("-", " ", gsub("_", " ", input$projectName) )  )
+
+    })
+
+
+    # compute Dir selection:
+    global <- reactiveValues(datapath = checkProgDir( getwd() )  ) # this sets initial val to current working DIR
+
+    # allows selection of Dir, with Volume set to HOME Dir
+    shinyDirChoose(
+      input,
+      'dir',
+      roots = c(home = '~'),
+      filetypes = c('', 'txt', 'bigWig', "tsv", "csv", "bw")
+    )
+
+    dir <- reactive(input$dir)
+
+    observeEvent(ignoreNULL = TRUE,
+                 eventExpr = {
+                   input$dir
+                 },
+                 handlerExpr = {
+                   if (!"path" %in% names(dir())) return()
+                   home <- normalizePath("~")
+                   global$datapath <-
+                     file.path(home, paste(unlist(dir()$path[-1]), collapse = .Platform$file.sep))
+                 })
+
+    observe({
+      if(global$datapath != "") {
+        global$datapath <- checkProgDir(global$datapath)
+      }
+    })
+
+    output$dir <- renderText({
+      global$datapath
+    })
+
+    observe({
+      if(global$datapath == "") {
+        output$warningDirectory <- renderText({
+          "DIR PATH NOT VALID PROGRAMME"
+        })
+      }
+      else {
+        output$warningDirectory <- renderText({
+          ""
+        })
+      }
+    })
+
+
+    observe({
+
+      if( global$datapath != "" && input$projectName != "" ) {
+
+        projPath <- getProjectPath(global$datapath, input$projectName )
+
+        output$projectPathOutput <- renderText({
+          projPath
+        })
+
+      }
+
+      else {
+
+        output$projectPathOutput <- renderText({
+          ""
+        })
+
+      }
+    })
+
+
+    observe({
+
+    if( grepl("\\s", input$projectName)  ) {
+      output$warningName <- renderText({
+        "PROJECT NAME CANNOT CONTAIN SPACES"
+      })
+    }
+      else {
+        output$warningName <- renderText({
+          ""
+        })
+      }
+    })
+
+
+
+    # perform computations to create new Programme:
+    observeEvent(input$done, {
+
+      if(input$projectName == "") {
+        # set the warningName TextOutput:
+        output$warningName <- renderText({
+          "*** PROVIDE PROJECT NAME ***"
+        })
+      }
+      else if( grepl("\\s", input$projectName)  ) {
+        # set the warningName TextOutput:
+        output$warningName <- renderText({
+          "*** PROJECT NAME CANNOT CONTAIN SPACES ***"
+        })
+      }
+      else if(global$datapath == "") {
+        output$warningDirectory <- renderText({
+          "*** DIR PATH NOT VALID PROGRAMME ***"
+        })
+      }
+      else {
+
+        # call projectmanagr::createProjectDoc:
+        projectmanagr::createProjectDoc(projectName = input$projectName,
+                                        projectTitle = input$projectTitle,
+                                        fileSystemPath = global$datapath,
+                                        projDocTemplate = "Project-Doc-Template.Rmd",
+                                        projectIndex = 0 )
+
+        # Close Gadget after 'done' is clicked.
+        stopApp()
+
+      }
+
+    })
+
+  }
+
+
+  viewer <- dialogViewer("Create New Project Document", width = 1000, height = 800)
+
+  runGadget(ui, server, viewer = viewer)
+
+
+}
