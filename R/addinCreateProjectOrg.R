67cc827763a2b92a44b07b785a2d36b302a3850f
stevenjwest <stevenjonwest@gmail.com>
2020-01-06 12:22:15 +0000

    Add addinCreateProjectOrg function
    This function provides a shiny interface to use via RStudio Addin the createProjectOrg() function of projectmanagr.

---
+++ R/addinCreateProjectOrg.R
@@ -0,0 +1,100 @@
+#' Create a New Project Org
+#'
+#' Generates a Shiny Gadget for initialising
+#' a new Project Organisation.  User selects a destination in
+#' the file system, Organisation name (for the directory) and
+#' Organisation title (for the html page).
+#'
+#' @export
+addinCreateProjectOrg <- function() {
+
+    ui <- miniPage(
+
+      gadgetTitleBar("Create New Project Org"),
+
+      miniContentPanel(
+
+        fillCol(
+
+          fillRow( h5("Initialise a new Project Organisation in Selected Directory.") ),
+
+          fillRow(  textInput("organisationName", "Organisation Directory Name:", value = "00_ORG", width="100%")  ),
+
+          fillRow(  span( textOutput("warning"), style="color:red")  ),
+
+          fillRow(  textInput("organisationTitle", "Organisation Title:", value = "ORGANISATION", width="100%")  ),
+
+          fillRow(   span( textOutput("warning2"), style="color:red")  ),
+
+          fillRow( flex = c(7, 1),  verbatimTextOutput("dir", placeholder = TRUE), shinyDirButton("dir", "Select Directory", "Organisation Parent Directory")  )
+
+        )
+      )
+    )
+
+    server <- function(input, output, session) {
+
+      global <- reactiveValues(datapath = getwd())
+
+      shinyDirChoose(
+        input,
+        'dir',
+        roots = c(home = '~'),
+        filetypes = c('', 'txt', 'bigWig', "tsv", "csv", "bw")
+      )
+
+      dir <- reactive(input$dir)
+
+      output$dir <- renderText({
+        global$datapath
+      })
+
+      observeEvent(ignoreNULL = TRUE,
+                   eventExpr = {
+                     input$dir
+                   },
+                   handlerExpr = {
+                     if (!"path" %in% names(dir())) return()
+                     home <- normalizePath("~")
+                     global$datapath <-
+                       file.path(home, paste(unlist(dir()$path[-1]), collapse = .Platform$file.sep))
+                   })
+
+
+      observeEvent(input$done, {
+
+        if(input$organisationName == "") {
+          # set the warning TextOutput:
+          output$warning <- renderText({
+            "PROVIDE ORGANISATION NAME"
+          })
+        }
+        else if(input$organisationTitle == "") {
+          # set the warning TextOutput:
+          output$warning2 <- renderText({
+            "PROVIDE ORGANISATION TITLE"
+          })
+        }
+        else {
+
+          # call projectmanagr::createProjectOrg:
+          projectmanagr::createProjectOrg( orgName = input$organisationName,
+                                           orgTitle = input$organisationTitle,
+                                           fileSystemPath = global$datapath )
+
+          # Close Gadget after 'done' is clicked.
+          stopApp()
+
+        }
+
+      })
+
+    }
+
+
+    viewer <- dialogViewer("Create New Project Organisation", width = 1000, height = 800)
+
+    runGadget(ui, server, viewer = viewer)
+
+
+}
