38d98a68571c10134eb320360dcccb29925f48dc
Steven J. West <stevenjwest@users.noreply.github.com>
2024-07-04 12:28:58 +0100

    move-functions : initial plan for new move_file() and move_dir() functions

--- R/move-functions.R
+++ R/move-functions.R
@@ -1,153 +1,43 @@
 
-#' Move a Project Note to new Directory
+#' Move a File to new Directory
 #'
-#' This Function moves a Project Note
+#' This Function moves a File (Project Note [simple, group] or Project Doc) to
+#' a new directory in the same organisation.
 #'
-#'  adds a Protocol to a Project Note - protocols are found in the
-#'  PROGRAMMES' protocols/ Dir.
+#' Updates all GDT links and hyperlinks across organisation.
 #'
-#' Inserted protocols add each Procedure defined under the '# PROTOCOL' section
-#'  of the document. Notes sections (FALSE by default) and the Equipment section
-#' (TRUE by default) can be optionally included in the inserted protocol.
+#' @param filePath Path to file to be moved.  Must be a Project Note
+#' [simple, group] or Project Doc.
 #'
-#' All links in the Protocol are UPDATED to work from the destination Project
-#'  Note.
-#'
-#' All graphics included in a knitr::include_graphics() r code chunk are
-#'  transferred to the new Project Note DIR and linked appropriately.
-#'
-#' @param protocolName The name of the Protocol Rmd file - WITHOUT extension.
-#'
-#' @param selection Selection object from the Project Note file where the protocol
-#' is to be inserted.  Use `projectmanagr::cursor_selection()` or
-#' `projectmanagr::user_selection()` to create this object.
-#'
-#' @param protocolInsertionTemplate Template file that contains boilerplate content
-#' for protocol insertion into project note.
+#' @param destinationDir Path to destination directory.  Must be in same
+#' Organisation.  If the directory does not exist, it will be created.
 #'
 #' @export
-move_project_note <- function(protocolName, selection,
-                            protocolInsertionTemplate="Protocol-Insertion-Template.Rmd") {
+move_file <- function(filePath, destinationDir) {
 
-  cat( "\nprojectmanagr::move_project_file():\n" )
+  cat( "\nprojectmanagr::move_file():\n" )
 
-
-  #### Set Instance Variables ####
-
-  projNoteRmdPath <- selection[["filePath"]] # presumed to be project note Rmd
-  noteInsertionIndex <- selection[["originalLineNumber"]]
-
-  # get orgPath
-  orgPath <- find_org_directory(projNoteRmdPath)
-
-  if(orgPath == "" ) { # only if orgPath not identified
-    stop( paste0("  Cannot identify organisation directory: ", projNoteRmdPath) )
-  }
-  # now, orgPath should be the root dir of the organisation
-
-  # get config templates settings yml
-  confPath <- get_config_dir(orgPath)
-  tempPath <- get_template_dir(orgPath)
-  settings <- get_settings_yml(orgPath)
-
-  # get the progPath:
-  progPath <- find_prog_dir(projNoteRmdPath)
-
-  # get protocol directory path:
-  #protocolsPath <- paste0(progPath, .Platform$file.sep, settings[["ProgrammeProtocolsDir"]])
-
-  # define protocol Dir path + Rmd path
-  protocolDirPath <- paste0( protocolsPath, .Platform$file.sep, protocolName)
-  protocolRmdPath <- paste0( protocolDirPath, .Platform$file.sep, protocolName, ".Rmd")
-
-  # define protocol title from name - remove - & _ and make first letter CAPITAL
-  protocolTitle <- gsub("-", " ", gsub("_", " ", protocolName) )
-  protocolTitle <- gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", protocolTitle, perl = TRUE)
+}
 
 
-  #### CHECK FOR ERRORS IN INPUT ####
 
-  # Check protocolName contains NO SPACES:
-  if( grepl("\\s+", protocolName) ) {
-    stop( paste0("  protocolName contains a SPACE: ", protocolName) )
-  }
+#' Move a Directory to new location
+#'
+#' This Function moves a directory containing Project Note(s) [simple, group]
+#' or Project Doc(s) to a new location.
+#'
+#' Updates all GDT links and hyperlinks across organisation.
+#'
+#' @param dirPath Path to directory to be moved.  Must not be the ORG root or
+#' a PROGRAMME directory.
+#'
+#' @param destinationDir Path to destination directory.  Must be in same
+#' Organisation.  If the directory does not exist, it will be created.
+#'
+#' @export
+move_dir <- function(dirPath, destinationDir) {
 
-  # check selection is a Project Note - simple or sub or head
-  if( selection[["rmdType"]] != "NOTE"
-      && selection[["rmdType"]] != "SUB"
-      && selection[["rmdType"]] != "HEAD" ) {
-    stop( paste0("  selection is not a Project Note: ", projNoteRmdPath) )
-  }
-
-  # Check protocolRmdPath doesnt exist
-  if( file.exists(protocolRmdPath) == FALSE ) {
-    stop( paste0("  protocol of this name doesn't exists: ", protocolRmdPath) )
-  }
-
-
-  #### Read Rmds ####
-
-  protocolContents <- read_file(protocolRmdPath)
-  protocolInsertionContents <- read_file( paste0( tempPath, .Platform$file.sep, protocolInsertionTemplate) )
-  projNoteContents <- read_file(projNoteRmdPath)
-
-
-  #### Create Links Protocol & Project Note ####
-
-  # create link from protocol to project note
-  protocolLink <- paste0(settings[["ProtocolLinkFormat"]],
-                         create_hyperlink( protocolName, protocolRmdPath, projNoteRmdPath),
-                         settings[["ProtocolLinkFormat"]])
-
-  #### Get Protocol from file ####
-
-  # compute location in protocolContents of protocol
-  headerLine <- grep_line_index(load_param_vector(settings[["ProtocolHeader"]], orgPath),
-                                     protocolContents)
-  sopHeaderLine <- grep_line_index_from(load_param_vector(settings[["ProtocolSopHeader"]], orgPath),
-                                     protocolContents, headerLine)
-  # get footerline by finding ProtocolResultsLogHeader first then working BACK to find ProtocolFooter
-  resultsLogLine <- grep_line_index_from(load_param_vector(settings[["ProtocolResultsLogHeader"]], orgPath),
-                                         protocolContents, sopHeaderLine)
-  pFooterVector <- load_param_vector(settings[["ProtocolFooter"]], orgPath)
-  fl <- grep_line_index_from_rev(pFooterVector, protocolContents, resultsLogLine)
-  footerLine <- fl - length(pFooterVector) # minus length of vector to remove actual footer line!
-
-
-  #### Replace markup in Protocol Insertion Template ####
-
-  protocolInsertionContents <- sub_template_param(protocolInsertionContents,
-                                        "{{PROTOCOL_INSERTION_SEP}}",
-                                        settings[["ProtocolInsertionSep"]],
-                                        orgPath)
-
-  protocolInsertionContents <- sub_template_param(protocolInsertionContents,
-                                        "{{PROTOCOL_INSERTION_TITLE}}",
-                                        paste0(settings[["ProtocolInsertionTitle"]], protocolTitle),
-                                        orgPath)
-
-  protocolInsertionContents <- sub_template_param(protocolInsertionContents,
-                                                  "{{PROTOCOL_INSERTION_LINK}}",
-                                                  protocolLink,
-                                                  orgPath)
-
-  protocolInsertionContents <- sub_template_param(protocolInsertionContents,
-                                                  "{{PROTOCOL_INSERTION_CONTENT}}",
-                                                  protocolContents[sopHeaderLine:footerLine],
-                                                  orgPath)
-
-
-  #### Add Protocol Insertion Template to Project Note
-
-  projNoteContents <- insert_at_indices(projNoteContents,
-                                        noteInsertionIndex, protocolInsertionContents)
-
-
-  #### write Header Note ####
-
-  write_file(projNoteContents, projNoteRmdPath)
-
-  cat( "  Inserted Protocol into Project Note: ", projNoteRmdPath, "\n" )
+  cat( "\nprojectmanagr::move_dir():\n" )
 
 }
 
